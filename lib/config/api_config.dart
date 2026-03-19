fcf29760ae729ab7bcad1042b30b1f6055430a60
sam-zarila <zarilatroxx99@gmail>
2026-03-19 23:49:20 +0200

    revenue model

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -14,7 +14,7 @@ class ApiConfig {
   /// - For Android emulator + backend on host: use http://10.0.2.2:3000
   /// - For physical device: use your PC's LAN IP, e.g. http://192.168.1.x:3000
   static const String _defaultProdRoot =
-      'https://unbigamous-unappositely-kory.ngrok-free.dev';
+      'https://d2bf-102-70-95-102.ngrok-free.app ';
   //  'https://7f74-102-70-95-217.ngrok-free.app ';
 
   // static const String _defaultProdRoot =
